d2811601a7afa829a342da56e07d8565ec7043be
Rachel B <rachelleeboland@gmail.com>
2024-10-29 19:23:17 +0000

    Preliminary scripts and update to battery prefab

---
+++ Assets/R Scripts/AudioBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //on door close turn off sounds from inside room
+}
